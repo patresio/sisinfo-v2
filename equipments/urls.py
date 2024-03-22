f79c7792cae8f0fc5c9d81754b7d1cec16704f1b
Ricardo Pereira <patresio@gmail.com>
2024-03-22 11:35:30 -0300

    feat: page equipment initial

--- equipments/urls.py
+++ equipments/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from equipments.views import equipment_register, equipments
+from equipments.views import equipment_register, equipment_view, equipments
 
 app_name = "equipments"
 
 
 urlpatterns = [
-    path("register_equipment", equipment_register, name="register_equipment"),
     path("", equipments, name="equipments"),
+    path("equipment/<slug:slug>", equipment_view, name="equipment_view"),
+    path("register_equipment", equipment_register, name="register_equipment"),
 ]
--- equipments/views.py
+++ equipments/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages import constants
 from django.core.paginator import Paginator
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
 from equipments.filters import EquipmentFilter
@@ -42,3 +42,12 @@ def equipments(request):
         "my_filter": my_filter,
     }
     return render(request, "equipments.html", context)
+
+
+@login_required(login_url="login")
+def equipment_view(request, slug):
+    equipment = get_object_or_404(Equipment, slug=slug)
+    context = {
+        "equipment": equipment,
+    }
+    return render(request, "equipment.html", context)
