59e6de1cea2de1610f9731ab3dfce205259b78fb
Ricardo Pereira <patresio@gmail.com>
2024-04-04 14:48:32 -0300

    feat: initial service

---
+++ services/forms.py
@@ -0,0 +1,65 @@
+from django import forms
+
+from authenticate.models import ProfessionalUser
+from services.models import Service
+
+
+class ServiceForm(forms.ModelForm):
+    class Meta:
+        model = Service
+        fiels = [
+            "number_service",
+            "call",
+            "fullname_employee",
+            "sector",
+            "room",
+            "professional",
+            "pro_accountable",
+            "status",
+            "remote",
+            "details",
+        ]
+
+        def clean_fullname_employee(self):
+            employee = self.cleaned_data["fullname_employee"]
+            words = [w.capitalize() for w in employee.split()]
+            return " ".join(words)
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request")  # store value of request
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            if field in [self.fields["details"]]:
+                field.widget.attrs["class"] = (
+                    "block p-2.5 px-0 mt-1 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                )
+                field.widget.attrs["rows"] = "4"
+            else:
+                field.widget.attrs["class"] = (
+                    "block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                )
+                if field in [self.fields["professional"]]:
+                    professional = ProfessionalUser.objects.filter(
+                        id=self.request.user.id
+                    )
+                    field.queryset = professional
+                    field.widget.attrs["value"] = self.request.user.id
+                    field.widget.attrs["selected"] = self.request.user.id
+                    field.widget.attrs["data-placeholder"] = self.request.user
+                    field.initial = self.request.user.id
+                if field in [self.fields["pro_accountable"]]:
+                    if (
+                        self.request.user.is_tech is True
+                        and self.request.user.username != "administrador"
+                    ):
+                        professional = ProfessionalUser.objects.filter(
+                            id=self.request.user.id
+                        )
+                        field.queryset = professional
+                        field.widget.attrs["value"] = self.request.user.id
+                        field.widget.attrs["selected"] = self.request.user.id
+                        field.widget.attrs["data-placeholder"] = self.request.user
+                        field.initial = self.request.user.id
+                    else:
+                        professional = ProfessionalUser.objects.filter(is_tech=True)
+                        field.queryset = professional
--- services/models.py
+++ services/models.py
@@ -1,75 +1,107 @@
-# from datetime import date, datetime
+from datetime import date, datetime
 
-# from django.db import models
-# from django.shortcuts import resolve_url as r
-# from django.template.defaultfilters import slugify
+from django.db import models
+from django.shortcuts import resolve_url as r
+from django.template.defaultfilters import slugify
 
-# from authenticate.models import ProfessionalUser
-# from dashboard.models import Sector
+from authenticate.models import ProfessionalUser
+from dashboard.models import Sector
+
+KIND_SERVICE_CALL = {
+    ("01", "WHATSAPP"),
+    ("02", "TELEFONE"),
+    ("03", "EMAIL"),
+    ("04", "PRESENCIAL"),
+    ("05", "VERBAL"),
+    ("99", "OUTROS"),
+}
 
 
-# # Create your models here.
-# class Service(models.Model):
-#     number_service = models.CharField(
-#         "identificação do atendimento",
-#         max_length=20,
-#         unique=True,
-#         blank=True,
-#         null=True,
-#     )
-#     slug = models.SlugField("slug")
-#     sector = models.ForeignKey(
-#         Sector,
-#         on_delete=models.DO_NOTHING,
-#         verbose_name="setor",
-#         related_name="setor",
-#     )
-#     room = models.CharField("sala referente", max_length=20, blank=True)
-#     details = models.TextField("detalhes do atendimento")
-#     fullname_employee = models.CharField(
-#         "nome completo do funcionario", max_length=100, null=True, blank=True
-#     )
-#     professional = models.ForeignKey(
-#         ProfessionalUser,
-#         on_delete=models.DO_NOTHING,
-#         verbose_name="profissional",
-#         related_name="profissional",
-#     )
-#     pro_accountable = models.ForeignKey(
-#         ProfessionalUser,
-#         on_delete=models.DO_NOTHING,
-#         verbose_name="profissional responsável",
-#         related_name="responsável",
-#     )
-#     status = models.BooleanField("status", default=True)
-#     created_at = models.DateTimeField(
-#         "criado em",
-#         auto_now_add=True,
-#     )
-#     updated_at = models.DateTimeField(
-#         "atualizado em",
-#         auto_now=True,
-#     )
+# Create your models here.
+class Service(models.Model):
+    number_service = models.CharField(
+        "identificação do atendimento",
+        max_length=20,
+        unique=True,
+        blank=True,
+        null=True,
+    )
+    call = models.CharField(
+        "tipo de atendimento", max_length=2, choices=KIND_SERVICE_CALL, default="02"
+    )
+    slug = models.SlugField("slug")
+    fullname_employee = models.CharField(
+        "nome completo do funcionario", max_length=100, null=True, blank=True
+    )
+    sector = models.ForeignKey(
+        Sector,
+        on_delete=models.DO_NOTHING,
+        verbose_name="setor",
+        related_name="setor",
+    )
+    room = models.CharField("sala referente", max_length=20, blank=True)
+    professional = models.ForeignKey(
+        ProfessionalUser,
+        on_delete=models.DO_NOTHING,
+        verbose_name="profissional",
+        related_name="profissional_atendimento",
+    )
+    pro_accountable = models.ForeignKey(
+        ProfessionalUser,
+        on_delete=models.DO_NOTHING,
+        verbose_name="profissional responsável",
+        related_name="profissional_responsavel_atendimento",
+    )
+    status = models.BooleanField("status", default=True)
+    remote = models.BooleanField("remoto", default=False)
+    created_at = models.DateTimeField(
+        "criado em",
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        "atualizado em",
+        auto_now=True,
+    )
+    details = models.TextField("detalhes do atendimento")
 
-#     class Meta:
-#         ordering = ["-created_at", "status", "-updated_at"]
-#         verbose_name = "atendimento"
-#         verbose_name_plural = "atendimentos"
+    class Meta:
+        ordering = ["-created_at", "status", "-updated_at"]
+        verbose_name = "atendimento"
+        verbose_name_plural = "atendimentos"
 
-#     def __str__(self):
-#         return self.number_service
+    def __str__(self):
+        return self.number_service
 
-#     def get_absolute_url(self):
-#         return r("services:service_view", slug=self.slug)
+    def get_absolute_url(self):
+        return r("services:service_view", slug=self.slug)
 
-#     def save(self, *args, **kwargs):
-#         service = Service.objects.filter(created_at__date=date.today()).count()
-#         if not self.slug:
-#             if not self.number_service:
-#                 self.number_service = (
-#                     datetime.now().strftime("%Y%m%d")
-#                     + f"{self.sector.id:03}"
-#                     + f"{(service + 1):03}"
-#                 )
-#             self.slug = slugify(self.number_service)
-#         return super().save()
+    def days_service_open(self):
+        """
+        Retorna a quantidade de dias que o atendimento está aberto
+        """
+        if self.created_at != self.updated_at:
+            if self.status:
+                return f"{str((date.today() - self.created_at.date()).days)}"
+            return "0"
+        if (date.today() - self.update_at.date()).days > -1:
+            return f"{str((date.today() - self.created_at.date()).days)}"
+
+    def days_service_closed(self):
+        """
+        Retorna quantidade de dias que o atendimento ficou aberto
+        """
+        if not self.status:
+            return f"{str((self.updated_at.date() - self.created_at.date()).days)}"
+        return False
+
+    def save(self, *args, **kwargs):
+        service = Service.objects.filter(created_at__date=date.today()).count()
+        if not self.slug:
+            if not self.number_service:
+                self.number_service = (
+                    datetime.now().strftime("%Y%m%d")
+                    + f"{self.sector.id:03}"
+                    + f"{(service + 1):03}"
+                )
+            self.slug = slugify(self.number_service)
+        return super().save()
